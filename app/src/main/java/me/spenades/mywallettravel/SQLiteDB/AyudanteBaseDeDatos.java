f951bb22f95c847e5fc1b367ef35165504f88a56
Sergio Penadés <spenades@gmail.com>
2024-02-22 21:08:43 +0100

    Añadidas las tablas restantes a la BD

--- app/src/main/java/me/spenades/mywallettravel/SQLiteDB/AyudanteBaseDeDatos.java
+++ app/src/main/java/me/spenades/mywallettravel/SQLiteDB/AyudanteBaseDeDatos.java
@@ -3,29 +3,49 @@ package me.spenades.mywallettravel.SQLiteDB;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-        //Crear Base de Datos Interna
+import android.os.Environment;
+
+//Crear Base de Datos Interna
 public class AyudanteBaseDeDatos extends SQLiteOpenHelper {
     private static final String
-            DB_PATH = "/data/data/me.spenades.mywallettravel/databases/",
             NOMBRE_BASE_DE_DATOS = "walletsDB",
-            NOMBRE_TABLA_WALLETS = "wallets",
-            NOMBRE_TABLA_TRANSACCIONES = "transacciones";
+            NOMBRE_TABLA_WALLETS = "wallet",
+            NOMBRE_TABLA_USUARIOS = "usuario",
+            NOMBRE_TABLA_CATEGORIAS = "categoria",
+            NOMBRE_TABLA_WALLETS_USUARIOS = "wallet_usuario",
+            NOMBRE_TABLA_TRANSACCIONES = "transaccion";
+
+
 
     private static final int VERSION_BASE_DE_DATOS = 1;
 
     public AyudanteBaseDeDatos(Context context) {
-        super(context, NOMBRE_BASE_DE_DATOS, null, VERSION_BASE_DE_DATOS);
+        super(context,
+                NOMBRE_BASE_DE_DATOS,
+                null,
+                VERSION_BASE_DE_DATOS);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s(id integer primary key autoincrement, nombre text, descripcion text, propietario int, compartir int)", NOMBRE_TABLA_WALLETS));
 
         db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s(id integer primary key autoincrement, descripcion text, importe int, pagador txt, participantes int, categoria txt, fecha int, walletId int)", NOMBRE_TABLA_TRANSACCIONES));
+
+        db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s(id integer primary key autoincrement, nombre text, apodo text, pin int)", NOMBRE_TABLA_USUARIOS));
+
+        db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s(id integer primary key autoincrement, wallet_id int, usuario_id int)", NOMBRE_TABLA_WALLETS_USUARIOS));
+
+        db.execSQL(String.format("CREATE TABLE IF NOT EXISTS %s(id integer primary key autoincrement, categoria text)", NOMBRE_TABLA_CATEGORIAS));
+
+
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
 }
